b24b9f04cd6509bd8a2e580512870f24831304f5
AbhasJha <73982312+AbhasJha@users.noreply.github.com>
2022-11-17 12:28:48 +0530

    Demo for Testing
    Demo

---
+++ Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Async="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript">
+        /*code: 48-57 Numbers*/
+        function restrictAlphabets(e) {
+            var x = e.which || e.keycode;
+            if ((x >= 48 && x <= 57))
+                return true;
+            else
+                return false;
+        }
+        
+
+        function setValue(node) {
+            var txtBoxValue = document.getElementById("txtNumber").value;
+            if (txtBoxValue > 1000) {
+                alert("Please Enter number between 1 to 1000");
+                document.getElementById("txtNumber").value = "";
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtNumber" AutoPostBack="true" OnTextChanged="txtNumber_TextChanged"   onkeypress='return restrictAlphabets(event)' onchange = "setValue(this);" TextMode="Number" MaxLength="4"  min="1" runat="server"></asp:TextBox>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator1" 
+runat="server" ErrorMessage="Enter only positive integer values."
+ControlToValidate="txtNumber" ValidationExpression="^[1-9][0-9]*$"
+CssClass="required" Display="Dynamic" />
+            <asp:GridView ID="GridView1" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowDataBound="GridView1_RowDataBound"   EmptyDataText="! Sorry No data found"
+                AutoGenerateColumns="true" PageSize="20" AllowPaging="True" runat="server"></asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private static string day = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack==true)
+        {
+
+        }
+
+    }
+
+    protected void txtNumber_TextChanged(object sender, EventArgs e)
+    {
+        int num =Convert.ToInt16(txtNumber.Text);
+        if (num < 1000)
+        {
+
+            loadGrid();
+
+
+        }
+        else
+        {
+            txtNumber.Text = "";
+            GridView1.DataSource = null;
+            GridView1.DataBind();
+        }
+    }
+
+    protected async void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            if (e.Row.Cells[0].Text == "fizz")
+            {
+                e.Row.Cells[0].ForeColor = System.Drawing.Color.Blue;
+                if(day=="T")
+                e.Row.Cells[0].Text = day + e.Row.Cells[0].Text.ToString().Substring(1, e.Row.Cells[0].Text.ToString().Length - 1);
+                
+            }
+            else if (e.Row.Cells[0].Text == "buzz")
+            {
+                e.Row.Cells[0].ForeColor = System.Drawing.Color.Green;
+                if (day == "T")
+                    e.Row.Cells[0].Text = day + e.Row.Cells[0].Text.ToString().Substring(1, e.Row.Cells[0].Text.ToString().Length - 1);
+                
+            }
+        }
+        
+    }
+    private void loadGrid()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("COL1", typeof(String));
+         day = DateTime.Now.ToString("ddd");
+        day = char.ToUpper(day[0]).ToString();
+        for (int i = 1; i <= Convert.ToInt16(txtNumber.Text); i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                dt.Rows.Add("fizz buzz");
+            }
+            else if (i % 3 == 0)
+            {
+              
+                dt.Rows.Add("fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                dt.Rows.Add("buzz");
+            }
+            else
+                dt.Rows.Add(Convert.ToInt16(i));
+        }
+       
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+    }
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+
+        loadGrid();
+    }
+}
